f065c32ac9f8463561ae14514e7ac830c00df9c4
Shawn Matyanga <87149956+starkjarvis21@users.noreply.github.com>
2023-10-16 20:20:24 +0200

    Update agriculture_chatbot.py

--- agriculture_chatbot.py
+++ agriculture_chatbot.py
@@ -1,65 +1,69 @@
 import streamlit as st
-import re
 import pandas as pd
 import numpy as np
 import pickle as pk
 import json
 import random
+import re
 from nltk.stem.porter import PorterStemmer
-from sklearn.feature_extraction.text import CountVectorizer
-from keras.utils import to_categorical
-from sklearn.preprocessing import LabelEncoder
 from keras.models import load_model
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
 import joblib
 
-dataset = pd.read_csv('intent.csv', names=["Intent"])
-y = dataset["Intent"]
+# Title for your Streamlit app
+st.title("Agriculture Chatbot")
 
-
-# Load Intent Model
+# Load the intent model and CountVectorizer
 loadedIntentClassifier = load_model('intent_model.h5')
 loaded_intent_CV = joblib.load('IntentCountVectorizer.sav')
 
-# Load Entity Model
+# Load entity CountVectorizer and classifier
 loadedEntityCV = pk.load(open('EntityCountVectorizer.sav', 'rb'))
 loadedEntityClassifier = joblib.load(open('entity_model.sav', 'rb'))
 
-labelencoder_intent = LabelEncoder()
-y = to_categorical(labelencoder_intent.fit_transform(y))
-intent_label_map = {cl: labelencoder_intent.transform([cl])[0] for cl in labelencoder_intent.classes_}
-
-
-# Load Intent Label Map and Intents JSON
+# Load intents.json
 with open('intents.json') as json_data:
     intents = json.load(json_data)
-intent_label_map = {cl: labelencoder_intent.transform([cl])[0] for cl in labelencoder_intent}
 
-st.title("Agriculture Chatbot")
+def getEntities(query):
+    query = loadedEntityCV.transform(query).toarray()
+    response_tags = loadedEntityClassifier.predict(query)
+    entity_list = [list(entity_label_map.keys())[list(entity_label_map.values()).index(tag)] for tag in response_tags if tag in entity_label_map.values()]
+    return entity_list
 
-user_query = st.text_input("Hello! How May I Help You Today? ")
+# Initialize variables
+USER_INTENT = ""
+entity_label_map = {}  # Define or load this as needed
+
+# User input field
+user_query = st.text_input("You: ")
+
+# Button to ask the chatbot
 if st.button("Ask"):
-    # Handle user query and display responses here
-    # You can use the existing code for processing queries and generating responses.
-    if user_query:
-        query = re.sub('[^a-zA-Z]', ' ', user_query).split(' ')
-        ps = PorterStemmer()
-        tokenized_query = [ps.stem(word.lower()) for word in query]
+    query = re.sub('[^a-zA-Z]', ' ', user_query).split(' ')
+    ps = PorterStemmer()
+    tokenized_query = [ps.stem(word.lower()) for word in query]
 
     processed_text = loaded_intent_CV.transform([' '.join(tokenized_query)]).toarray()
     predicted_Intent = loadedIntentClassifier.predict(processed_text)
     result = np.argmax(predicted_Intent, axis=1)
 
+    USER_INTENT = ""
     for key, value in intent_label_map.items():
         if value == result[0]:
             USER_INTENT = key
             break
 
+    response = ""
     for i in intents['intents']:
         if i['tag'] == USER_INTENT:
             response = random.choice(i['responses'])
-            st.write("Chatbot:", response)
 
     entities = getEntities(tokenized_query)
     token_entity_map = dict(zip(entities, tokenized_query))
-    # Display entities or other information as needed
 
+    # Display the chatbot's response
+    st.write(f"Bot: {response}")
+
+# Additional Streamlit configurations and UI elements can be added as needed
